2a94988337c868af6edf21d8a834c8fef7cb03bf
Chelsea  Dobbins <luckyluxe21@gmail.com>
2023-03-12 05:26:46 +0000

    method

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -26,6 +26,12 @@ public class Lab {
      * @return text.
      */
     public String copy(String text){
-        return "return text please!";
+        return text;
+        
+        
+        
+        
+
+
     }
 }
